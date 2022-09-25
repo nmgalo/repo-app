f967b8306784a5a327245f6376b90e967856c6fa
Nika <nikolozamgalo@gmail.com>
2022-09-25 15:01:32 +0400

    added unit tests

--- app/src/test/java/dev/nmgalo/repo/domain/repo/SaveToFavouritesUseCaseTest.kt
+++ app/src/test/java/dev/nmgalo/repo/domain/repo/SaveToFavouritesUseCaseTest.kt
@@ -8,6 +8,8 @@ import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -21,15 +23,36 @@ class SaveToFavouritesUseCaseTest {
 
     private val fakeRepo = RepoDetails("", "", "")
 
+    private val flowOfNull = flowOf(null)
+
+    private val fakeRepoDetails = RepoDetails(
+        owner = "nmgalo",
+        avatar = "https://avatars.githubusercontent.com/u/33604954?v=4",
+        repositoryName = "repo-app"
+    )
+
+    @Before
     fun setUp() {
         coEvery { repo.getDetails("", "") } returns fakeRepo
-        coEvery { getLocalRepoDetailsUseCase.execute("", "") } returns flowOf(null)
+        coEvery { getLocalRepoDetailsUseCase.execute("test", "ninja") } returns flowOfNull
+        coEvery {
+            getLocalRepoDetailsUseCase.execute("nmgalo", "repo-app")
+        } returns flowOf(fakeRepoDetails)
         coEvery { repo.saveToFavourites(fakeRepo) } returns Unit
     }
 
     @Test
-    fun `should write in database when there isn't existing record`() = runTest {
-        getLocalRepoDetailsUseCase.execute("", "")
+    fun `should return flow of null if records not found in database`() = runTest {
+        assertEquals(getLocalRepoDetailsUseCase.execute("test", "ninja"), flowOfNull)
     }
 
+    @Test
+    fun `should return valid repository if exists`() = runTest {
+        getLocalRepoDetailsUseCase.execute("nmgalo", "repo-app").collect {
+            assertEquals(
+                it?.avatar,
+                fakeRepoDetails.avatar
+            )
+        }
+    }
 }
